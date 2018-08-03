c2ebfd7c266eee4cc4fc9e5ba936ff509b8bd212
Sergiu Dauceanu <sergiu.dauceanu@googlemail.com>
2018-08-03 16:23:38 +0200

    Chapter 10 Email Support

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_mail import Mail
 from config import Config
 from flask_login import LoginManager
 import logging
@@ -14,6 +15,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+mail = Mail(app)
 
 from app import routes, models, errors
 
