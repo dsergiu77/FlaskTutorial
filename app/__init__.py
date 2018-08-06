b66d623ad2d1b32f6bea76425bc4197353bc18bb
Sergiu Dauceanu <sergiu.dauceanu@googlemail.com>
2018-08-06 11:30:28 +0200

    Chapter 12, Datetimes

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 from config import Config
 from flask_login import LoginManager
 import logging
@@ -19,6 +20,8 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
+
 
 from app import routes, models, errors
 
