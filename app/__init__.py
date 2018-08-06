46d573c3fd16e02af2931b205ed2a39b4e21a54a
Sergiu Dauceanu <sergiu.dauceanu@googlemail.com>
2018-08-06 10:30:44 +0200

    Chapter 11 Facelift/Bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 from config import Config
 from flask_login import LoginManager
 import logging
 from logging.handlers import SMTPHandler
 from logging.handlers import RotatingFileHandler
 import os
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -16,6 +18,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 from app import routes, models, errors
 
